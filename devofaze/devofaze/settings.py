fcf50b649af90ffc63ba80790e32f008a6fb0a38
huseynovfuad <55555038+huseynovfuad@users.noreply.github.com>
2020-03-29 12:51:00 +0400

    update settings

--- devofaze/devofaze/settings.py
+++ devofaze/devofaze/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l3k-c+(j+ce)q6%+(rtjhamed_0csmgtj_kn-8&8-lls9%e@l0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
